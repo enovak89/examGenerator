3fac0d71007bba591de73d7ef7fcb31bf7306609
enovak89 <enovak89@mail.ru>
2023-04-04 19:58:31 +0300

    Init cpommit

---
+++ src/test/java/com/example/examgenerator/ExamGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
